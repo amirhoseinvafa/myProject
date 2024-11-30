2531cb17798518a6cae425e9fc8e2571bab6225e
z.rezaie <z.rezaie@gmail.com>
2024-11-30 23:55:07 +0330

    myapp

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import {BrowserRouter as Router,Route,Routes, Link} from 'react-router-dom'
+import { Login } from './components/Login'
+import { ToDoList } from './components/ToDoList'
+import { Register } from './components/Register'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <Router>
+          <Link to="/Login">Login </Link>
+          <Link to="/ToDoList">ToDoList </Link>
+          <Link to="/Register">Register </Link>
+          <Routes>
+            <Route path='/Login' element={<Login/>} />
+            <Route path='/ToDoList' element={<ToDoList/>} />
+            <Route path='/Register' element={<Register/>} />
+          </Routes>
+        </Router>
+
+      </div>
+    </>
+  )
+}
+
+export default App
